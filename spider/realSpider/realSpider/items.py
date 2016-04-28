905ffe1e96450ea0d3447ee4f7c5c67f1d93df6f
Real_Sanqian <shadows0717@gmail.com>
2016-04-28 11:16:18 +0800

    哎哟哎哟加班到十点

--- spider/realSpider/realSpider/items.py
+++ spider/realSpider/realSpider/items.py
@@ -11,7 +11,9 @@ import scrapy
 class RealspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
     pass
+
+class DmozItem(scrapy.Item):
+    title = Field()
+    link = Field()
+    url = Field()

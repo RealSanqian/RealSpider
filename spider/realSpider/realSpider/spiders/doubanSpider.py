e809d011c332b2d6e5608574717b90fa8736074e
Real_Sanqian <shadows0717@gmail.com>
2016-04-28 21:17:46 +0800

    写日报，回家。官网demo跑成功

---
+++ spider/realSpider/realSpider/spiders/doubanSpider.py
@@ -0,0 +1,13 @@
+import scrapy
+class doubanSpider(scrapy.Spider):
+    name = "doubanSpider"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)

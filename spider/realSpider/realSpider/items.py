e2d266d66665b4da62ed54d4dc890ab67406a0f5
Real_Sanqian <shadows0717@gmail.com>
2016-05-09 09:59:36 +0800

    新建新闻item

--- spider/realSpider/realSpider/items.py
+++ spider/realSpider/realSpider/items.py
@@ -4,7 +4,6 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
@@ -14,6 +13,17 @@ class RealspiderItem(scrapy.Item):
     pass
 
 class DmozItem(scrapy.Item):
-    title = Field()
-    link = Field()
-    url = Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    url = scrapy.Field()
+    pass
+
+class NewsItem(scrapy.Item):
+    name = scrapy.Field() #网站名称
+    title = scrapy.Field() #新闻标题
+    detail = scrapy.Field() #文章内容
+    url = scrapy.Field() #网站地址
+    link = scrapy.Field() #爬虫爬取地址
+    time = scrapy.Field() #爬虫爬取时间
+    vn = scrapy.Field() #爬虫爬取次数
+
